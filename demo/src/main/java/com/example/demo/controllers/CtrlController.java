0dad9c0ba87d0e38bca674d0ea2772513c5e6dac
Yuzunoha <studysteadystandby@gmail.com>
2023-01-29 20:07:07 +0900

    オプショナル

--- demo/src/main/java/com/example/demo/controllers/CtrlController.java
+++ demo/src/main/java/com/example/demo/controllers/CtrlController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controllers;
 
+import java.util.Optional;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,5 +21,11 @@ public class CtrlController {
   @ResponseBody
   public String param(@PathVariable String id) {
     return "id: " + id;
+  }
+
+  @GetMapping({ "/paramAny/{id}", "/paramAny" })
+  @ResponseBody
+  public String paramAny(@PathVariable(required = false) Optional<String> id) {
+    return "id: " + id.orElse("パスパラメタ id は指定されませんでした。");
   }
 }
