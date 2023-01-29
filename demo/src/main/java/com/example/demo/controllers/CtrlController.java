4fc92abf718760b4a02315e5c2b5d572ab352fde
Yuzunoha <studysteadystandby@gmail.com>
2023-01-30 00:26:24 +0900

    アップロードできるようになりました。

--- demo/src/main/java/com/example/demo/controllers/CtrlController.java
+++ demo/src/main/java/com/example/demo/controllers/CtrlController.java
@@ -1,6 +1,9 @@
 package com.example.demo.controllers;
 
 import com.example.demo.models.MemberForm;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Optional;
 import java.util.function.UnaryOperator;
 import org.springframework.stereotype.Controller;
@@ -10,7 +13,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 @RequestMapping("/ctrl")
 @Controller
@@ -43,6 +48,30 @@ public class CtrlController {
   @PostMapping("/form")
   public String formResult(@ModelAttribute MemberForm memberForm, Model model) {
     model.addAttribute("main", "ctrl/form_result::main");
+    return "common/layout";
+  }
+
+  @GetMapping("/upload")
+  public String upload(Model model) {
+    model.addAttribute("main", "ctrl/upload::main");
+    return "common/layout";
+  }
+
+  @PostMapping("/upload")
+  public String upload(
+    Model model,
+    @RequestParam("upfile") MultipartFile file
+  ) {
+    String name = file.getOriginalFilename();
+    try (
+      var bof = new BufferedOutputStream(new FileOutputStream("./" + name))
+    ) {
+      bof.write(file.getBytes());
+      model.addAttribute("success", name + "のアップロードに成功しました");
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    model.addAttribute("main", "ctrl/upload::main");
     return "common/layout";
   }
 }
