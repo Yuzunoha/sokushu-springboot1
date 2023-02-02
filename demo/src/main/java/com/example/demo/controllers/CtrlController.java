35cc8edf0b7eb0c7620561e83568c0e24bf064f3
Yuzunoha <studysteadystandby@gmail.com>
2023-02-02 19:26:27 +0900

    JSON取れた

--- demo/src/main/java/com/example/demo/controllers/CtrlController.java
+++ demo/src/main/java/com/example/demo/controllers/CtrlController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.Book;
 import com.example.demo.models.MemberForm;
 import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -95,5 +97,25 @@ public class CtrlController {
   @ResponseBody
   public String reqheader(@RequestHeader String accept) {
     return accept;
+  }
+
+  @GetMapping("/json")
+  public String getJson() {
+    return "ctrl/json";
+  }
+
+  @PostMapping("/json")
+  @ResponseBody
+  public String json(@RequestBody Book book) {
+    String s = "";
+    String d = " ... ";
+    s += book.getId() + d;
+    s += book.getIsbn() + d;
+    s += book.getTitle() + d;
+    s += book.getPrice() + d;
+    s += book.getPublisher() + d;
+    s += book.getPublished() + d;
+    s += book.getAttach() + d;
+    return s;
   }
 }
